874c7d6123b0307c37d8e218688fc22f5b15d81a
rattumourya <160098742+rattumourya@users.noreply.github.com>
2024-02-27 13:11:50 +0000

    Reacting to events

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,3 +1,12 @@
-export default function TabButton({children}) {
-    return <li><button>{children}</button></li>
+export default function TabButton({ children }) {
+
+    function clickHandler() {
+        console.log("Hello World!");
+    }
+
+    return (
+    <li>
+        <button onClick={clickHandler} >{children}</button>
+    </li>
+    );
 }
