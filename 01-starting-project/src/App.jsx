30bd1205976f05fa4955b0f3a48a5a20ab09b5ff
rattumourya <160098742+rattumourya@users.noreply.github.com>
2024-02-27 12:21:48 +0000

    Storing Components Style Files Next To Components

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,33 +1,7 @@
-import reactImg  from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data.js';
-const reactDescription = ['Fundamental','crucial','Core'];
+import Header from "./components/Header.jsx"
+import CoreConcepts from './components/CoreConcepts.jsx';
 
-function getRandomInt(max)
-{
-  return Math.floor(Math.random()*(max+1));
-}
-function Header() {
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {reactDescription[getRandomInt(2)]} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  )
-}
-
-function CoreConcepts({title,description,image}) {
-  return (
-    <li>
-        <img src={image} alt={title} />
-        <h3>{title}</h3>
-        <p>{description}</p>
-    </li>
-  )
-}
 function App() {
   return (
     <div>
