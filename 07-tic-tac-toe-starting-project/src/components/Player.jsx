cb25f76a684dadf661fbaa41c5aa6552c64b9bf1
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-03-11 03:45:04 +0000

    User input and two way binding

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,22 +1,27 @@
 import { useState } from "react"
 
-export default function Player({name,symbol}) {
+export default function Player({initialName,symbol}) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing,setIsEditing] = useState(false);
 
     let buttonName = "Edit";
-    let playerField = <span className="player-name">{name}</span>;
+    let playerField = <span className="player-name">{playerName}</span>;
 
     if(isEditing)
     {
          buttonName = "Save";
-         playerField = <input type="text" defaultValue ={name}  required/>
+         playerField = <input type="text" value={playerName} onChange={handleChange} required/>
     }
 
     function editNameButtonHandler()
     {
         setIsEditing((editing) => !editing); // => schedules a state update to true
-
     }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
     return (
         <li>
             <span className="player">
