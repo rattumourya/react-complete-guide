c3140e134dbf9dd693955d2be92978e687b6e1e9
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-03-16 03:09:41 +0000

    Adding a header component

---
+++ investment-calculator/src/components/UserInput.jsx
@@ -0,0 +1,25 @@
+
+function UserInput() {
+    return (
+        <div id="user-input">
+            <div className="input-group">
+                <label htmlFor="">INITIAL INVESMENT</label>
+                <input id="" type="number" />
+            </div>
+            <div className="input-group" >
+                <label htmlFor="">INITIAL INVESMENT</label>
+                <input id="" type="number" />
+            </div>
+            <div className="input-group">
+                <label htmlFor="">INITIAL INVESMENT</label>
+                <input id="" type="text" />
+            </div>
+            <div className="input-group" >
+                <label htmlFor="">INITIAL INVESMENT</label>
+                <input id="" type="text" />
+            </div>
+        </div>
+    )
+}
+
+export default UserInput
