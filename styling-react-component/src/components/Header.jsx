dd1b4cb32016399b9725bff91660cb056789a0cd
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-03-20 04:57:21 +0000

    Inline styling

--- styling-react-component/src/components/Header.jsx
+++ styling-react-component/src/components/Header.jsx
@@ -5,7 +5,10 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p style={{
+        color: "blue",
+        textAlign: "left"
+      }}>A community of artists and art-lovers.</p>
     </header>
   );
 }
