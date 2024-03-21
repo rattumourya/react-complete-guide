ba5f7ce1a1a6ceef7e972cb8216acdb8096015a7
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-03-21 05:13:18 +0000

    Adding font-title in the header

--- styling-react-component/src/components/Header.jsx
+++ styling-react-component/src/components/Header.jsx
@@ -47,8 +47,8 @@ export default function Header() {
   return (
     <header className="flex flex-col items-center mt-8 mb-16">
       <img src={logo} className="mb-8 w-44 h-44 object-contain" alt="A canvas" />
-      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800">ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">ReactArt</h1>
+      <p className='text-stone-500'>A community of artists and art-lovers.</p>
     </header>
   );
 }
