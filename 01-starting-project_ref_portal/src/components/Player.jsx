9a22d76cbcd102501513f2ef1f021eed85202493
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-03-24 11:04:44 +0000

    Handle name change and submit event

--- 01-starting-project_ref_portal/src/components/Player.jsx
+++ 01-starting-project_ref_portal/src/components/Player.jsx
@@ -1,10 +1,21 @@
+import {useState} from "react";
+
 export default function Player() {
+
+  const [playerName,setPlayerName]  = useState('')
+  const [isSubmitted,setSubmit] = useState(false);
+
+  function handleChange(event){
+    setPlayerName(event.target.value);
+    setSubmit(false);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {isSubmitted ? playerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} />
+        <button onClick={() => setSubmit(true)}>Set Name</button>
       </p>
     </section>
   );
