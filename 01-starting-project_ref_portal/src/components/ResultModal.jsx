61252aad415148c6ed70ef4568033112fad0d5e5
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-03-25 11:37:27 +0000

    Portal used  place  JSX code in modal of  dom

--- 01-starting-project_ref_portal/src/components/ResultModal.jsx
+++ 01-starting-project_ref_portal/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal } from 'react-dom';
 
 
 const ResultModal = forwardRef(function ResultModal({remainingTime,targetTime,onReset},ref){
@@ -16,7 +17,7 @@ const ResultModal = forwardRef(function ResultModal({remainingTime,targetTime,on
         }
     })
 
-    return (
+    return createPortal(
         <dialog ref={dialog} className="result-modal">
             {userLost && <h2>You lost</h2>}
             {!userLost && <h2>Your score:  {score}</h2>}
@@ -25,7 +26,8 @@ const ResultModal = forwardRef(function ResultModal({remainingTime,targetTime,on
             <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById("modal")
     )
 });
 
