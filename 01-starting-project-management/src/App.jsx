58b1c5866a5138860a83f888b2d3f8650ae329f4
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-03-27 07:31:38 +0000

    Projects structure configured

--- 01-starting-project-management/src/App.jsx
+++ 01-starting-project-management/src/App.jsx
@@ -1,8 +1,16 @@
+import AddProjects from "./components/AddProjects";
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+      <div className="h-dvh bg-gray-50 flex flex-row">
+          <div className="grid grid-flow-col basis-1/3">
+            <div className="bg-slate-950 rounded-tr-lg self-end text-white p-2" style={{height: '95%'}}>
+               <h2>Add Projects</h2>
+            </div>
+          </div>
+          <div className="bg-gray-50 basis-2/3  ml-4">
+             
+          </div>
+      </div>
   );
 }
 
---
+++ 01-starting-project-management/src/components/AddProjects.jsx
@@ -0,0 +1,9 @@
+
+export default function AddProjects() {
+
+    return (
+        <span className="h-screen w-[28rem] mt-14 bg-stone-500 ">
+            <h2>YOUR PROJECTS</h2>
+        </span>
+    )
+}
