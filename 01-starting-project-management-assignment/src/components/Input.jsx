c428209460a80b1f8f99c800879a40d498b50c83
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-04-09 07:00:44 +0000

    Styling Button and Inputs With Tailwind CSS

--- 01-starting-project-management-assignment/src/components/Input.jsx
+++ 01-starting-project-management-assignment/src/components/Input.jsx
@@ -1,7 +1,7 @@
 export default function Input({label,textarea,...props}) {
 
-    return <p>
-          <label htmlFor="title">{label}</label>
-          {textarea ? <textarea {...props} /> : <input {...props} /> }
+    return <p className="flex flex-col my-4">
+          <label className="text-sm font-bold uppercase text-stone-500" htmlFor="title">{label}</label>
+          {textarea ? <textarea className="w-full p-1 rounded-sm border-b-2 border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" {...props} /> : <input className="w-full p-1 rounded-sm border-b-2 border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" {...props} /> }
     </p>
 }
