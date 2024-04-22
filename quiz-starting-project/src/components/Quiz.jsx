6487162ad8c59a7c03f6ab364911eea0597a51db
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-04-22 09:16:30 +0000

    Shuffling Answers And Adding Quiz Logic

--- quiz-starting-project/src/components/Quiz.jsx
+++ quiz-starting-project/src/components/Quiz.jsx
@@ -1,10 +1,25 @@
 import QUESTIONS from '../question.js';
 import { useState } from 'react';
+import quizComplete from '../assets/quiz-complete.png';
 
 export default function Quiz() {
 
     const [selectedAnswer, setSelectedAnswers] = useState([]);
     const activeQuestionIndex = selectedAnswer.length;
+    const isQuizCompleted = activeQuestionIndex === QUESTIONS.length;
+
+    if(isQuizCompleted) {
+        return (
+            <div id="summary">
+                <img src={quizComplete} alt="Quiz Completed" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        )
+    }
+
+
+    const suffledQuestions = [...QUESTIONS[activeQuestionIndex].answers];
+    suffledQuestions.sort((q1,q2) =>  Math.random() - 0.5);
 
     function handleSelectAnswer(selectedAnswer) {
         setSelectedAnswers((prevSelectedAnswers) => {
